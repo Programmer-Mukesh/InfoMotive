4383cdaa5c692759d3cdda38840293e25b4efe47
Mukesh <mukeshkumar989898@gmail.com>
2022-10-12 19:02:20 +0530

    corrected home page

--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,11 @@
 import { Container, Grid, Paper } from "@mui/material";
 import Head from "next/head";
-import { useState } from "react";
 
 export default function Home() {
-  const [input, setInput] = useState({});
-
-  const handleChange = (event) => {
-    const objKey = event.target.name;
-    const objValue = event.target.value;
-
-    setInput({ ...input, [objKey]: objValue });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    let data = {
-      Fee: {
-        general: input?.generalFee,
-        OBC: input?.obcFee,
-        SC: input?.scFee,
-        ST: input?.stFee,
-        female: input?.femaleFee,
-      },
-      age: {
-        min: input?.minAge,
-        max: input?.maxAge,
-      },
-      totalPostCatWise: {
-        UR: input?.URPosts,
-        BC: input?.BCPosts,
-        MBC: input?.MBCPosts,
-        EWS: input?.EWSPosts,
-        SC: input?.SCPosts,
-        ST: input?.STPosts,
-        total: input?.totalPosts,
-      },
-      dispHeading: input?.dispHeading,
-      descriptionURL: input?.descriptionURL,
-      postName: input?.postName,
-      postDate: input?.postDate,
-      examDate: input?.examDate,
-      lastDate: input?.lastDate,
-      shortInfo: input?.shortInfo,
-      admitCardAvlbl: input?.admitCardAvlbl,
-      postNameShortDtl: input?.postNameShortDtl,
-      totalPost: input?.totalPost,
-      eligibility: input?.eligibility,
-      applyLink: input?.applyLink,
-      notificationLink: input?.notificationLink,
-      officialWebsiteLink: input?.officialWebsiteLink,
-    };
-
-    fetch("https://jobsall.herokuapp.com/api/govt/postjob", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Cache: "no-cache",
-      },
-    })
-      .then((response) => {
-        console.log(response);
-
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   return (
     <Container className="homeContainer">
       <Head>
-        <title>InfoMotive</title>
+        <title>InfoMotive | HomePage</title>
         <meta
           name="description"
           content="One stop solution for all jobs and results notification"
