35eaff6af53dac06d6743871dab4db7ab15c378d
Mukesh <mukeshkumar989898@gmail.com>
2022-10-16 22:13:40 +0530

    ui fixes

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "@mui/material";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className="HeaderContainer">
       <Container className="headerFlexBox">
-        <div className="title">
+        <div className="title" onClick={() => router.push("/")}>
           Info<span className="titleSpan">Motive</span>
         </div>
         <ul className="navItems">
